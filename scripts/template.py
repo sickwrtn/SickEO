06177d86f1f751056c06bc97bf366e1496d0289a
sickwrtn <sillo154265@gmail.com>
2025-04-07 22:10:11 +0900

    DRLAW

--- scripts/template.py
+++ scripts/template.py
@@ -31,14 +31,16 @@ class ExtensionTemplateScript(scripts.Script):
                 return [checkbox]
         
         def process(self, p, *args):
+
                 print("process Event")
 
         def process_before_every_sampling(self, p, *args, **kwargs):
                 print("process_before_every_sampling")
 
         def process_before_every_step(self, p, *args, **kwargs):
+                print(kwargs);
                 print("process_before_every_step")
-                
+
         def run(self, p, angle, checkbox):
                 proc = process_images(p)
                 print(proc,angle,checkbox)
